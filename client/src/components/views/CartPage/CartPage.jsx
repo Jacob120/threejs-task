06b020a4d0c02c2876426e1d41df17fb2324daf7
Jakub Kozik <jakubkozik120@gmail.com>
2023-01-09 20:58:01 +0100

    Fix heroku upload

--- client/src/components/views/CartPage/CartPage.jsx
+++ client/src/components/views/CartPage/CartPage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CartContext from '../../../context/CartContext';
 import { API_URL } from '../../../config';
--- server.js
+++ server.js
@@ -66,8 +66,15 @@ app.use(
   })
 );
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const server = app.listen(process.env.PORT || 8000, () => {
